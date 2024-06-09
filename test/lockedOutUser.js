1e2e8c1ce9f2ba173fde6e6d7f9640c96c28d653
Tmehnaz <tanjinamehanz@gmail.com>
2024-06-10 02:48:59 +0300

    3rd task added

---
+++ test/lockedOutUser.js
@@ -0,0 +1,32 @@
+const {Builder, Key, By, until} = require('selenium-webdriver');
+
+const chai = import('chai');
+const { expect } = chai;
+
+let driver
+
+//describe block
+
+describe('Add item to cart test', async function(){
+    
+ 
+    
+    it("Login", async function(){
+
+        driver = await new Builder().forBrowser("chrome").build();
+
+        await driver.get("https://www.saucedemo.com/checkout-complete.html");
+
+        await driver.findElement(By.id("user-name")).sendKeys("locked_out_user", Key.RETURN);
+        await driver.findElement(By.id("password")).sendKeys("secret_sauce",Key.RETURN);
+        await driver.findElement(By.id("login-button")).click();
+        
+        
+        let element = await driver.wait(until.elementLocated(By.className("error-button")), 10000)
+
+        let elementTxt = await element.getText();
+        let errorMsg = await elementTxt.isDisplayed();
+        expect(errorMsg).to.be.true;
+          
+    });
+});
